859022c288e76ffd2d51328355efcf271d0aca17
= <=>
2023-03-03 22:38:46 +0700

    Farhan Dwi Pramana

---
+++ resources/views/program.blade.php
@@ -0,0 +1,6 @@
+<html>
+<h1>Program</h1>
+<ul><a href="program/karir">Karir</a></ul>
+<ul><a href="program/magang">Magang</a></ul>
+<ul><a href="program/kunjungan">Kunjungan Industri</a></ul>
+</html>
